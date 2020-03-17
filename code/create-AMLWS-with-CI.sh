98ff9316fb5dcc329919838bb4b6989747bd5c83
SeokJin Han <sehan@microsoft.com>
2020-03-17 16:26:42 +0900

    fix name mismatch
    Will use resource group and workspace names from the external file.

--- code/create-AMLWS-with-CI.sh
+++ code/create-AMLWS-with-CI.sh
@@ -5,8 +5,7 @@
 ### set common variables
 region={REPLACE THIS}
 subscription_id={REPLACE THIS}
-resource_group_name={REPLACE THIS}
-amlws_name={REPLACE THIS}
+# Resource Group names and AML Workspace names will be from the external file
 computeinstance_name={REPLACE THIS}
 vm_size={REPLACE THIS}
 ssh_public_access={REPLACE THIS} # `true` if needed
@@ -24,6 +23,8 @@ for workspaceName in `cat workspaceNames.txt`; do
     az deployment group create --resource-group $workspaceName --template-file ./azuredeploy.json -p workspaceName=$workspaceName -p location=koreacentral -p sku=basic
 
     # Step2: Create Compute Instances using Python SDK
-    python ./computeInstanceClient.py --operation create --subscription-id $subscription_id --resource-group-name $resource_group_name --workspace-name $amlws_name --compute-instance-name $computeinstance_name --vm-size $vm_size --ssh-public-access $ssh_public_access --admin-user-ssh-public-key "$admin_user_ssh_public_key"
+    echo ==================================================
+    echo Start creating Compute Instance: $computeinstance_name, under Azure ML Workspace: $workspaceName
+    python ./computeInstanceClient.py --operation create --subscription-id $subscription_id --resource-group-name $workspaceName --workspace-name $workspaceName --compute-instance-name "$workspaceName-$computeinstance_name" --vm-size $vm_size --ssh-public-access $ssh_public_access --admin-user-ssh-public-key "$admin_user_ssh_public_key"
 done
 
